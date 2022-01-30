004d4a7febf87ad8929a781a17a96e577ac83d35
FranSantander <naturatiendacopiapo@gmail.com>
2022-01-29 22:48:06 -0300

    Segundo commit subiendo index2 con metodo forEach

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 //funcion constructora consultorio
 function Consultorio(nombre, paciente) {
-    var nombre = nombre;
-    var paciente = paciente;
+    var _nombre = nombre;
+    var _paciente = paciente;
 };
 Object.defineProperty(this, "_getNombreConsultorio",{
     get: function () {
-        return nombre
+        return _nombre
     },
 });
 Object.defineProperty(this, "_setNombreConsultorio", {
-    set: function (nombre) {
-        nombre = nombre
+    set: function (nuevoNombre) {
+        _nombre = nuevoNombre
     }
 });
 Object.defineProperty(this, "_getPaciente",{
     get: function () {
-        return paciente
+        return _paciente
     },
 });
 Object.defineProperty(this, "_setPaciente", {
-    set: function (paciente) {
-        paciente = paciente
+    set: function (nuevoPaciente) {
+        _paciente = nuevoPaciente
     }
 });
+
+//Prototype Consultorio
+Consultorio.prototype.getNombreConsultorio = function (){
+    return this._nombre
+}
+Consultorio.prototype.getPacientes = function(){
+    this._paciente.forEach(function(element, index){
+console.log("El nombre del paciente es: " + (element), "y el número es: " +(index+1));
+    })
+    console.log(this._paciente)
+};
+//Prototype Paciente
+/*Consultorio.prototype.agregarPaciente = function (paciente){
+this._paciente.push(paciente)
+}
+//Prototype buscar pacientes
+Cosnultorio.prototype.getNombre = function (){
+    const result = this._paciente.filter
+}*/
+
+
+
 //funcion constructora Paciente
 function Paciente(nombre, edad, rut, diagnostico) {
     var _nombre = nombre;
@@ -36,8 +58,8 @@ Object.defineProperty(this, "_getNombrePaciente", {
     }
 });
 Object.defineProperty(this, "_setNombrePaciente", {
-    set: function (nombre) {
-       _nombre = nombre
+    set: function (nuevoNombrePaciente) {
+       _nombre = nuevoNombrePaciente
     }
 });
 Object.defineProperty(this, "_getEdad", {
@@ -46,8 +68,8 @@ Object.defineProperty(this, "_getEdad", {
     }
 });
 Object.defineProperty(this, "_setEdad", {
-    set: function (edad) {
-       __edad = edad
+    set: function (nuevEdad) {
+       __edad = nuevEdad
     }
 });
 Object.defineProperty(this, "_getRut", {
@@ -56,8 +78,8 @@ Object.defineProperty(this, "_getRut", {
     }
 });
 Object.defineProperty(this, "_setRut", {
-    set: function (rut) {
-       __rut = rut
+    set: function (nuevoRut) {
+       __rut = nuevoRut
     }
 });
 Object.defineProperty(this, "_getDiagnostico", {
@@ -66,16 +88,87 @@ Object.defineProperty(this, "_getDiagnostico", {
     }
 });
 Object.defineProperty(this, "_setDiagnostico", {
-    set: function (diagnostico) {
-       __diagnostico = diagnostico
+    set: function (nuevoDiagnostico) {
+       __diagnostico = nuevoDiagnostico
     }
 });
 
-//Prototype
+//
+/*Consultorio.prototype.getPacientePorNombre = function (nombre) {
+    this.paciente().forEach(function(element,index) {
+        if (element._nombre() == busquedaNombre){
+            console.log("Paciente Nº"+(index+1));
+            console.log(element._nombre());
+        }
+    });
+}*/
+
+//instancia new
+var p1 = new Paciente ("Claudia Rojas", 45, "14.444.444-4", "Celiaca");
+var p2 = new Paciente ("Daniel Ulloa", 38, "15.555.555-5", "diabetes");
+var p3 = new Paciente ("Fernanda Castex", 25, "19.999.999-9", "hipertensión");
+var p4 = new Paciente ("Cristian Sarmineto", 27, "18.888.888-8", "hipercolestemia");
+var p5 = new Paciente ("Francisca Santander", 34, "16.666.666-6", "ar");
+var p6 = new Paciente ("Flora", 55, "11.111.111-1", "depresión" )
+console.log(p1, p2, p3, p4, p5)
+
+
+//console.log(consul1.getNombrePaciente());
+//console.log(consul1.getNombreConsultorio());
+//consul1.getPacientePorNombre("Claudia Rojas")
+
+
+var consul1 = new Consultorio("Ñuñoa", [p1, p2, p3, p4, p5]);
+console.log(consul1);
+console.log(consul1.getNombreConsultorio()) 
+
+consul1.agregarPaciente(p6)
+
+console.log(consul1.getPacientes());
+
+
+
+
+
+/*Consultorio.prototype.mostrarTodosLosPacientes = function () {
+	this._getNombrePaciente.map((paciente) => {
+		console.log("Nombre: " + paciente._getNombrePaciente);
+		console.log("Edad: " + paciente._getEdad);
+		console.log("Rut: " + paciente._getRut);
+		console.log("Diagnostico: " + paciente._getDiagnostico);
+	});
+};
 Consultorio.prototype._getPaciente = function() {
     return this.nombre
+};
+Paciente.prototype._getNombrePaciente = function () {
+    return this._nombre
+};
+Paciente.prototype._getEdad = function () {
+    return this._edad
+};
+Paciente.prototype._getRut = function () {
+    return this._rut
+};
+Paciente.prototype._getDiagnostico = function () {
+    return this._diagnostico
+};
+
+/*var registro = [
+    { _nombre: nombre,
+     _edad: edad,
+    _rut: rut,
+    _diagnostico: diagnostico}
+];
+registro.forEach (function (registro2) {
+    console.log(registros2._nombre, registro2_edad, registro2._rut, registros2._diagnostico)
+});*/
+
+/*var registro = ["_getNombrePaciente" + "_getEdad" + "_getRut" +  "_getDiagnostico"];
+Paciente.prototype.forEach = function forEach (registro) {
+   return this.registro
 }
-var registro = ["getNombrePaciente" + "_getEdad" + "_getRut" +  "_getDiagnostico"];
-Paciente.prototype.forEach = function forEach () {
-    
-}
+console.log(registro)*/ 
+
+
+
---
+++ script2.js
@@ -0,0 +1,115 @@
+//funcion constructora consultorio
+function Consultorio(nombre, paciente) {
+    var _nombre = nombre;
+    var _paciente = paciente;
+
+    Object.defineProperty(this, "_nombreConsultorio", {
+        get: function () {
+            return _nombre
+        },
+        set: function (nuevoNombre) {
+            _nombre = nuevoNombre
+        }
+    })
+    Object.defineProperty(this, "_nombrePaciente", {
+        get: function () {
+            return _paciente
+        }
+    })
+
+}
+
+Consultorio.prototype.getNombreConsultorio = function () {
+    return this._nombreConsultorio
+}
+
+Consultorio.prototype.getPacientes = function () {
+    this._nombrePaciente.forEach(function (element, index) {
+        console.log("El nombre del paciente es:  ".concat(element._nombrePaciente, " y el número es:  ").concat(index + 1));
+    })
+    console.log(this._nombrePaciente)
+}
+Consultorio.prototype.agregarPaciente = function (paciente){
+    this._nombrePaciente.push(paciente)
+}
+
+//Método prototype para mostrar todos los datos del paciente
+Consultorio.prototype.getNombre = function (buscarNombre){
+     const result = this._nombrePaciente.filter(element =>{
+        if(element._nombrePaciente == buscarNombre){
+            console.log(`El nombre del paciente es: ${element._nombrePaciente} y su rut es: ${element._rut}`)
+        }
+    })
+}
+
+Consultorio.prototype.getRut = function (buscarRut){
+    const result = this._nombrePaciente.filter(element =>{
+       if(element._rut == buscarRut){
+           console.log(`Búsqueda por rut: ${element._nombrePaciente} y su rut es: ${element._rut}`)
+       }
+   })
+}
+
+
+//funcion constructora Paciente
+function Paciente(nombre, edad, rut, diagnostico) {
+    var _nombre = nombre;
+    var _edad = edad;
+    var _rut = rut;
+    var _diagnostico = diagnostico || [];
+
+    Object.defineProperty(this, "_nombrePaciente", {
+        get: function () {
+            return _nombre
+        },
+        set: function (nuevoNombre) {
+            _nombre = nuevoNombre
+        }
+    })
+    Object.defineProperty(this, "_edad", {
+        get: function () {
+            return _edad
+        },
+        set: function (nuevaEdad) {
+            _edad = nuevaEdad
+        }
+    })
+    Object.defineProperty(this, "_rut", {
+        get: function () {
+            return _rut
+        },
+        set: function (nuevoRut) {
+            _rut = nuevoRut
+        }
+    })
+    Object.defineProperty(this, "_diagnostico", {
+        get: function () {
+            return _diagnostico
+        },
+        set: function (nuevoDiagnostico) {
+            _diagnostico = nuevoDiagnostico
+        }
+    })
+}
+
+//Instancia new
+var p1 = new Paciente("Claudia", 46, "14.444.444-4", "fiebre");
+var p2 = new Paciente("Daniel", 37, "15.555.555-5", "diabetes");
+var p3 = new Paciente("Fernanda", 28, "18.888.888-8", "hipertensión");
+var p4 = new Paciente("Cristian", 25, "19.999.999-9", "gripe");
+var p5 = new Paciente("Francisca", 34, "16.666.666-6", "celiaquía");
+var p6 = new Paciente("push Henry", 10, "23.333.333-3", "asma")
+
+
+
+//console.log(p1)
+
+consultorio1 = new Consultorio("Santa Elvira", [p1, p2, p3, p4, p5]);
+//console.log(consultorio1)
+//console.log(consultorio1.getNombreConsultorio())
+
+consultorio1.agregarPaciente(p6)
+
+console.log(consultorio1.getPacientes())
+console.log(consultorio1.getNombre("Francisca"))
+console.log(consultorio1.getRut("18.888.888-8"))
