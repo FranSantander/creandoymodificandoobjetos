27226c4c056cd80ee2c8f5d624f1aba16373b4a2
FranSantander <naturatiendacopiapo@gmail.com>
2022-01-29 23:26:43 -0300

    tercer commit desafio terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Todo en ES5
 //funcion constructora consultorio
 function Consultorio(nombre, paciente) {
     var _nombre = nombre;
@@ -23,6 +24,7 @@ Consultorio.prototype.getNombreConsultorio = function () {
     return this._nombreConsultorio
 }
 
+//Método prototype para mostrar todos los pacientes
 Consultorio.prototype.getPacientes = function () {
     this._nombrePaciente.forEach(function (element, index) {
         console.log("El nombre del paciente es:  ".concat(element._nombrePaciente, " y el número es:  ").concat(index + 1));
@@ -33,22 +35,22 @@ Consultorio.prototype.agregarPaciente = function (paciente){
     this._nombrePaciente.push(paciente)
 }
 
-//Método prototype para mostrar todos los datos del paciente
+//Método prototype para buscar los datos de los usuarios por nombre
 Consultorio.prototype.getNombre = function (buscarNombre){
-     const result = this._nombrePaciente.filter(element =>{
+     const result = this._nombrePaciente.filter(function (element){
         if(element._nombrePaciente == buscarNombre){
-            console.log(`El nombre del paciente es: ${element._nombrePaciente} y su rut es: ${element._rut}`)
+            console.log("El nombre del paciente es: " + element._nombrePaciente + ", su edad es: " + element._edad + ", su rut es: " + element._rut + " y su diagnostico: " + element._diagnostico);
         }
     })
 }
 
-Consultorio.prototype.getRut = function (buscarRut){
+/*Consultorio.prototype.getRut = function (buscarRut){
     const result = this._nombrePaciente.filter(element =>{
        if(element._rut == buscarRut){
            console.log(`Búsqueda por rut: ${element._nombrePaciente} y su rut es: ${element._rut}`)
        }
    })
-}
+}*/
 
 
 //funcion constructora Paciente
@@ -98,7 +100,7 @@ var p2 = new Paciente("Daniel", 37, "15.555.555-5", "diabetes");
 var p3 = new Paciente("Fernanda", 28, "18.888.888-8", "hipertensión");
 var p4 = new Paciente("Cristian", 25, "19.999.999-9", "gripe");
 var p5 = new Paciente("Francisca", 34, "16.666.666-6", "celiaquía");
-var p6 = new Paciente("push Henry", 10, "23.333.333-3", "asma")
+var p6 = new Paciente("push Prueba", 10, "23.333.333-3", "asma")
 
 
 
@@ -111,5 +113,5 @@ consultorio1 = new Consultorio("Santa Elvira", [p1, p2, p3, p4, p5]);
 consultorio1.agregarPaciente(p6)
 
 console.log(consultorio1.getPacientes())
-console.log(consultorio1.getNombre("Francisca"))
-console.log(consultorio1.getRut("18.888.888-8"))
+console.log(consultorio1.getNombre("Daniel"))
+//console.log(consultorio1.getRut("18.888.888-8"))
